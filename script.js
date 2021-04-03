4df0535dfda4bf8c0d6b0d7290fc95272a3070bc
Vishal Kumar Saw <vishalkrsaw2001@gmail.com>
2021-04-03 21:42:17 +0530

    navbar scroll code refactor

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const links = document.querySelectorAll('.nav-link');
 
+// adding scroll feature to navbar 
+let scrollInterVal;
 for (let link of links) {
     link.addEventListener('click', (e) => {
         e.preventDefault();
         const destinationID = link.getAttribute('href');
         const destination = document.querySelector(destinationID);
-        let targetPos = destination.getBoundingClientRect().y;
-        let currentPos = link.getBoundingClientRect().y;
-        let scrollInterVal = setInterval(() => {
-            if (currentPos >= targetPos) {
-                clearInterval(scrollInterVal);
-                return;
-            }
-            currentPos += 50;
-            window.scrollBy(0, 50);
-        }, 40);
-    })
+        scrollInterVal = setInterval(scrollVertically, 40, destination);
+    });
 }
+
+const scrollVertically = (destination) => {
+    let targetPos = destination.getBoundingClientRect().top;
+    if (targetPos <= 0) {
+        clearInterval(scrollInterVal);
+        return;
+    }
+    window.scrollBy(0, 50);
+}
+
+
