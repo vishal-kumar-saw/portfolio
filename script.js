bd669995920ed2609c85e068e5470b789ebc51ca
Vishal Kumar Saw <vishalkrsaw2001@gmail.com>
2021-04-04 11:23:03 +0530

    add auto fill to all skill bars

--- script.js
+++ script.js
@@ -1,23 +1,67 @@
 const links = document.querySelectorAll('.nav-link');
 
 // adding scroll feature to navbar 
-let scrollInterVal;
 for (let link of links) {
     link.addEventListener('click', (e) => {
         e.preventDefault();
         const destinationID = link.getAttribute('href');
         const destination = document.querySelector(destinationID);
-        scrollInterVal = setInterval(scrollVertically, 40, destination);
-    });
+        let targetPos = destination.getBoundingClientRect().y;
+        let currentPos = link.getBoundingClientRect().y;
+        let scrollInterVal = setInterval(() => {
+            if (currentPos >= targetPos) {
+                clearInterval(scrollInterVal);
+                return;
+            }
+            currentPos += 50;
+            window.scrollBy(0, 50);
+        }, 30);
+    })
 }
 
-const scrollVertically = (destination) => {
-    let targetPos = destination.getBoundingClientRect().top;
-    if (targetPos <= 0) {
-        clearInterval(scrollInterVal);
-        return;
+
+// skill bar auto fill
+
+const progressBars = document.querySelectorAll('.skill-bars');
+const skillsContainer = document.getElementById('skills-container');
+
+const isVisible = (element) => {
+    const position = element.getBoundingClientRect();
+    if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        return true;
     }
-    window.scrollBy(0, 50);
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        return true;
+    }
+    return false;
 }
+
+let animationDone = false;
+
+const initialiseBars = () => {
+    for (let bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+const fillBars = () => {
+    for (let bar of progressBars) {
+        bar.style.transition = 'all 0.5s linear';
+        bar.style.width = bar.getAttribute('value') + '%';
+    }
+}
+initialiseBars();
+window.addEventListener('scroll', () => {
+    if (!animationDone && isVisible(skillsContainer)) {
+        fillBars();
+        animationDone = true;
+    }
+
+    if (isVisible(document.querySelector('.nav-menu'))) {
+        initialiseBars();
+        animationDone = false;
+    }
+});
+
 
 
